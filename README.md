cacadad513cb44cd952b6f2fc0642c40d6abaa7f
fabirt <fandig27@gmail.com>
2019-03-07 15:37:23 -0500

    platformer game

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Love2D
 Learning Love2D and Lua for game develop
+
+### Tile Map Editor
+Tiled: https://www.mapeditor.org/
--- platformerGame/conf.lua
+++
@@ -1,5 +0,0 @@
-function love.conf(t)
-    t.window.width  = 800
-    t.window.height = 400
-    t.console       = true
-end
