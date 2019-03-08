bd68cd77642d1ecf3ee7c72354007037ea4eaf38
fabirt <fandig27@gmail.com>
2019-03-07 20:08:38 -0500

    playing music

--- making-tile-map/main.lua
+++ making-tile-map/main.lua
@@ -2,7 +2,11 @@ require "tools/tiledmap"
 
 
 function love.load()
-    _G.map = loadTiledMap("assets/maps/map1/tilemap", "assets/maps/map1/")    
+    _G.map = loadTiledMap("assets/maps/map1/tilemap", "assets/maps/map1/")   
+    bgMusic = love.audio.newSource("assets/music/track1.mp3", "stream") 
+    bgMusic:setLooping(true)
+    bgMusic:setVolume(0.33)
+    bgMusic:play()
 end
 
 function love.draw()
